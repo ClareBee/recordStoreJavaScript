96e896c27dfeed53b60f63d522064b4200727345
Clare <chblackburne@gmail.com>
2017-12-02 11:24:38 +0000

    add record constructor

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -6,16 +6,16 @@ describe("record test", function(){
   beforeEach(function(){
     record = new Record("C Duncan", "Say", "Alternative", 6);
   });
-  xit('should have an artist', function(){
+  it('should have an artist', function(){
     assert.strictEqual(record.artist, "C Duncan");
   });
-  xit('should have a title', function(){
+  it('should have a title', function(){
     assert.strictEqual(record.title, "Say");
   });
-  xit('should have a genre', function(){
-    assert.strictEqual(record.genre, "Alterntative");
+  it('should have a genre', function(){
+    assert.strictEqual(record.genre, "Alternative");
   });
-  xit('should have a price', function(){
+  it('should have a price', function(){
     assert.strictEqual(record.price, 6);
   });
 })
