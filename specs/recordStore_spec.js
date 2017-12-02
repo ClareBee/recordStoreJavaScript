c29ba447c3be780d0c88c68708e2aafbb4998935
Clare <chblackburne@gmail.com>
2017-12-02 11:38:44 +0000

    record store constructor, passing tests

--- specs/recordStore_spec.js
+++ specs/recordStore_spec.js
@@ -1,22 +1,23 @@
 var assert = require('assert');
-var RecordStore = require('../recordstore.js');
+var RecordStore = require('../recordStore.js');
 var Record = require('../record.js');
 
 describe('record store tests', function(){
     var record, recordstore;
+
   beforeEach(function(){
-    var record = new Record("Bat For Lashes", "Daniel", "Alternative", 8);
-    var recordstore = new RecordStore("Big Al's", "Glasgow", []);
+    record = new Record("Bat For Lashes", "Daniel", "Alternative", 8);
+    recordstore = new RecordStore("Big Al's", "Glasgow", []);
   });
 
-  xit('should have a name', function(){
+  it('should have a name', function(){
     assert.strictEqual(recordstore.name, "Big Al's");
   });
 
-  xit('should have a city', function(){
+  it('should have a city', function(){
     assert.strictEqual(recordstore.city, "Glasgow");
   });
-  xit('should have an empty inventory', function(){
+  it('should have an empty inventory', function(){
     assert.deepStrictEqual(recordstore.inventory, []);
   });
 })
