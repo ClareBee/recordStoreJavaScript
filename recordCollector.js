11a99a7bbcbfb12a2180ee89feb52ae12daa173e
Clare <chblackburne@gmail.com>
2017-12-02 14:31:51 +0000

    pending tests for record collector added

--- recordCollector.js
+++ recordCollector.js
@@ -3,3 +3,17 @@ var RecordCollector = function(name){
   this.collection = [];
   this.cash = 0;
 }
+
+RecordCollector.prototype = {
+  sellRecord: function(record){
+    if(_.includes(this.collection, record) && record.collection > 1){
+        record.quantity -= 1;
+      }
+    else if(_.includes(this.collection, record) && record.quantity === 1){
+        _.remove(this.collection, record);
+      }
+    this.cash += record.price;
+  }
+}
+
+module.exports = RecordCollector;
--- specs/recordCollector_spec.js
+++ specs/recordCollector_spec.js
@@ -1,12 +1,23 @@
 var assert = require('assert');
 var RecordCollector = require('../recordCollector.js');
+var Record = require('../record.js');
 
 describe('record collector', function(){
-  
+
   beforeEach(function(){
     bob = new RecordCollector("Bob");
+    record = new Record("Say", "C Duncan", "Alternative", 8);
   });
   it('should have a name', function(){
     assert.strictEqual(bob.name, "Bob");
+  });
+  it('should have a collection of records', function(){
+    bob.getRecord(record);
+    assert.strictEqual(bob.collection.length, 1);
+  });
+  it('should be able to sell a record', function(){
+    bob.sellRecord(record);
+    assert.strictEqual(bob.cash, 8);
+    assert.strictEqual(bob.collection.recordNum(), 0);
   });
 })
